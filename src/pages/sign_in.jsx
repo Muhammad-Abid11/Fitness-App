0f7f15fea2ccf135b74ac6232cf090c7363aa613
Muhammad-Abid11 <abidkhan11845@Gmail.com>
2023-05-24 00:24:00 +0500

    first commit

---
+++ src/pages/sign_in.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+
+const SignInPage = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value);
+    };
+
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+    };
+
+    const handleSignIn = (e) => {
+        e.preventDefault();
+        // Perform sign-in logic here, such as calling an API or validating credentials
+        console.log('Email:', email);
+        console.log('Password:', password);
+        // Reset the form
+        setEmail('');
+        setPassword('');
+    };
+
+    return (
+        <div>
+            <h2>Sign In</h2>
+            <form onSubmit={handleSignIn}>
+                <div>
+                    <label htmlFor="email">Email:</label>
+                    <input
+                        type="email"
+                        id="email"
+                        value={email}
+                        onChange={handleEmailChange}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="password">Password:</label>
+                    <input
+                        type="password"
+                        id="password"
+                        value={password}
+                        onChange={handlePasswordChange}
+                    />
+                </div>
+                <button type="submit">Sign In</button>
+            </form>
+        </div>
+    );
+};
+
+export default SignInPage;
